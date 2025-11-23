fda1af7cf5727bfbbae4ae37fba542a183edac6a
Atif Sheikh <128314115+Sheikhatiff@users.noreply.github.com>
2025-11-23 21:28:36 +0500

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,10 @@ const ENVIRONMENT = process.env.NODE_ENV || "development";
 
 // CORS Configuration
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: origin: [
+      process.env.FRONTEND_URL || "http://localhost:5173",
+      process.env.CLIENT_URL || "http://localhost:5173",
+    ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
@@ -31,7 +34,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.get("/", (_, res) => {
+app.get("/api", (_, res) => {
   res.send(
     `Server started running at http://localhost:${PORT} at ${ENVIRONMENT} mode.`
   );
